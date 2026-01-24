84b420586029ee172b80d3ea46e6f9927c66a668
1teamimpact <impact219programmers@gmail.com>
2026-01-24 08:59:25 -0500

    Add harvester; remove shooter command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,12 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.ShooterSubsytem;
-import frc.robot.commands.ShooterCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class RobotContainer {
 
   private final CommandXboxController joystick = new CommandXboxController(0);
   private final ShooterSubsytem shooter = new ShooterSubsytem();
-  private final ShooterCommand shoot = new ShooterCommand(shooter);
 
   public RobotContainer() {
     configureBindings();
@@ -25,14 +23,20 @@ public class RobotContainer {
   }
 
   private void bindJoystickX() {
-     joystick.x().whileTrue(shoot); //hold x down to shoot using ShooterCommand
 
     // hold x down to shoot using Command API factorie
-    // joystick.x().whileTrue(
-    //     shooter.startEnd(
-    //         () -> shooter.runShooter(), // Start action
-    //         () -> shooter.stopShooter() // End action
-    //     ));
+    joystick.x().whileTrue(
+        shooter.startEnd(
+            () -> shooter.runShooter(), // Start action
+            () -> shooter.stopShooter() // End action
+        ));
+
+    // hold x down to shoot using Command API factorie
+    joystick.x().whileTrue(
+        shooter.startEnd(
+            () -> shooter.runShooter(), // Start action
+            () -> shooter.stopShooter() // End action
+        ));
   }
 
   public Command getAutonomousCommand() {
---
+++ src/main/java/frc/robot/subsystems/HarvesterSubsystem.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class HarvesterSubsystem extends SubsystemBase {
+  private final XboxController controller = new XboxController(0);
+  // private final TalonFX shooter  = new TalonFX(10);
+  private final SparkFlex harvester  = new SparkFlex(12, MotorType.kBrushless);
+
+
+  public void runShooter(){
+    // shooter.set(-.3);
+    harvester.set(.3);
+  }
+
+  public void stopShooter(){
+    harvester.set(0);
+  }
+
+
+
+  /** Creates a new ShooterSubsytem. */
+  public HarvesterSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
